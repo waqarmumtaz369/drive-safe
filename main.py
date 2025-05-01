b2f8eb3d043a2d320fcc4630bc6f99fcc9e5cd39
Waqar Mumtaz <waqar.mumtaz@gmail.com>
2025-05-01 23:45:57 +0500

    Change the logic only to calculate overall frame processing time

--- main.py
+++ main.py
@@ -55,6 +55,9 @@ if __name__ == "__main__":
 
     frame_count = 0
     while True:
+        # --- START TIMER BEFORE INFERENCE ---
+        start_time = time.time()
+
         ret, frame = cap.read()
         if not ret:
             print("Finished processing video or cannot read frame from camera.")
@@ -65,20 +68,17 @@ if __name__ == "__main__":
         # Resize the frame if width > 640, maintaining aspect ratio
         frame = resize_image(frame)
 
-         # --- START TIMER BEFORE INFERENCE ---
-        start_time = time.time()
-
         # Perform detection
         detections = detect_objects_and_seatbelt(frame, person_model, phone_model, seatbelt_model)
 
         # --- END TIMER AFTER INFERENCE ---
-        inference_time = time.time() - start_time
-        fps = 1.0 / inference_time if inference_time > 0 else 0
+        frame_processing_time = time.time() - start_time
+        fps = 1.0 / frame_processing_time if frame_processing_time > 0 else 0
 
         # Log results to terminal
         print(f"--- Frame {frame_count} Detections ---")
         # Log inference time and FPS
-        print(f"Inference time: {inference_time:.3f} sec | FPS: {fps:.2f}")
+        print(f"Frame Processing time: {frame_processing_time:.3f} sec | FPS: {fps:.2f}")
         if not detections:
             print("  No persons detected.")
         for i, det in enumerate(detections):
