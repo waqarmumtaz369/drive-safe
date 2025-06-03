b7644f40d7b508d87eadad0bc36c58d5900ada82
Waqar Mumtaz <waqar.mumtaz@gmail.com>
2025-06-03 22:30:25 +0500

    Update logic document and queue size

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ CLASS_NAMES_SEATBELT = {0: "Not Worn", 1: "Worn"}
 RESIZE_WIDTH = 800
 
 # --- Centralized Detection & Violation System Config ---
-QUEUE_DURATION = 5  # seconds for violation queue
+QUEUE_DURATION = 7  # seconds for violation queue
 TIMES_SEATBELT_DETECTED = 50  # percent threshold for seatbelt detection
 TIMES_PHONE_DETECTED = 50     # percent threshold for phone detection
 FPS_ESTIMATE_DEFAULT = 15     # fallback FPS if can't estimate
